5516039ca030ef24efe28cdb843099bf7fbf385b
Erwan Cloitre <erwan.cloitre@gmail.com>
2020-10-08 11:55:09 +0200

    version2.0

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Jeff Tuche</title>
+    <!-- STYLE -->
+    <link rel="stylesheet" media="screen and (min-width: 1025px)" href="css/index.css">
+    <link rel="stylesheet" media="screen and (max-width: 1024px)" href="css/small.css">
+    <!-- NAVBAR RESPONSIVE & FOOTER-->
+    <link rel="stylesheet" media="screen and (min-width: 1025px)" href="navbar_footer_CSS/header.css">
+    <link rel="stylesheet" media="screen and (max-width: 1024px)" href="navbar_footer_CSS/burgerMenu.css">
+    <link rel="stylesheet" href="navbar_footer_CSS/footer.css">
+</head>
+<body>
+<section class="first">
+    <?php include('header.php'); ?>
+<main>
+    <h1 class="name">Jeff Tuche</h1>
+    <video controls autoplay>
+        <source src="./video/tuche.mp4" type="video/mp4">
+    </video>
+</main>
+</section>
+    <?php include('footer.php'); ?>
+</body>
+</html>
---
+++ quisuis-je.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Jeff Tuche</title>
+    <!-- STYLE -->
+    <link rel="stylesheet" media="screen and (min-width: 1025px)" href="css/index.css">
+    <link rel="stylesheet" media="screen and (max-width: 1024px)" href="css/small.css">
+    <!-- NAVBAR RESPONSIVE & FOOTER-->
+    <link rel="stylesheet" media="screen and (min-width: 1025px)" href="navbar_footer_CSS/header.css">
+    <link rel="stylesheet" media="screen and (max-width: 1024px)" href="navbar_footer_CSS/burgerMenu.css">
+    <link rel="stylesheet" href="navbar_footer_CSS/footer.css">
+</head>
+<body>
+<section class="secondpage">
+    <?php include('header.php'); ?>
+    <main class="colonne">
+        <img class="logo" src="img/jeff.png">
+        <h1 class="title">QUI SUIS-JE?</h1>
+        <p class="descri">Et moi, c'est Tuche, avec un T.
+            Comme T'es là. T'es là, t'es là ou t'es pas là ?
+            T'es pas là ? Ben où c'est qu't'es ? Où c'est qu't'es ? T'es pas là ?
+            T'es pas là ? Où c'est qu't'es ? Ah, t'es là !
+            T'es là ou t'es pas là avec un T, comme Tuche, comme T'es là. Tuche, t'es là
+            </p>
+        <h1 class="title">CONTACT</h1>
+        <p class="coordo">Jeff Tuche</p>
+        <p class="coordo">5 route de Rocquigny</p>
+        <p class="coordo">Bouzolles, 59219 </p>
+        <p class="coordo">06 15 03 45 67 86</p>
+    </main>
+</section>
+    <?php include('footer.php'); ?>
+</body>
+</html>
